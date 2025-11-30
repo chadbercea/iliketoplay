4f7d64bcb9779d141cd21d54c85f66de80093af2
Chad Bercea <chadbercea@gmail.com>
2025-11-29 20:53:12 -0700

    Add RAWG image domain to Next.js config
    Configure Next.js Image component to allow images from media.rawg.io
    for game cover images from RAWG API.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.rawg.io',
+        pathname: '/media/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
 
